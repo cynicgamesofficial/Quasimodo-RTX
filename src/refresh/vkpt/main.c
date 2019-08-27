cynicgamesofficial/Quasimodo-RTX

commit 89387a27b39f1849be46671e89560c9939c402b5

Added light sampling correction based on statistical per-cluster light visibility. The idea is, if we see that a light is usually not visible, let's not sample it so much.

src/refresh/vkpt/main.c
@@ -190,6 +190,7 @@ VkResult
 vkpt_destroy_all(VkptInitFlags_t destroy_flags)
 {
 	vkDeviceWaitIdle(qvk.device);
+
 	for(int i = LENGTH(vkpt_initialization) - 1; i >= 0; i--) {
 		VkptInit_t *init = vkpt_initialization + i;
 		if((init->flags & destroy_flags) != destroy_flags)
@@ -209,6 +210,8 @@ vkpt_destroy_all(VkptInitFlags_t destroy_flags)
 	{
 		destroy_transparency();
 	}
+
+	vkpt_light_stats_destroy();
 
 	return VK_SUCCESS;
 }
@@ -1237,13 +1240,13 @@ static inline uint32_t fill_model_instance(const entity_t* entity, const model_t
 static void
 add_dlights(const dlight_t* lights, int num_lights, QVKUniformBuffer_t* ubo)
 {
-	ubo->num_lights = 0;
+	ubo->num_sphere_lights = 0;
 
 	for (int i = 0; i < num_lights; i++)
 	{
 		const dlight_t* light = lights + i;
 
-		float* dynlight_data = (float*)(ubo->dynamic_light_data + ubo->num_lights * 2);
+		float* dynlight_data = (float*)(ubo->dynamic_light_data + ubo->num_sphere_lights * 2);
 		float* center = dynlight_data;
 		float* radius = dynlight_data + 3;
 		float* color = dynlight_data + 4;
@@ -1253,7 +1256,7 @@ add_dlights(const dlight_t* lights, int num_lights, QVKUniformBuffer_t* ubo)
 		VectorScale(light->color, light->intensity / 500.f, color);
 		*radius = light->radius;
 
-		ubo->num_lights++;
+		ubo->num_sphere_lights++;
 	}
 }
 
@@ -1953,6 +1956,7 @@ prepare_ubo(refdef_t *fd, mleaf_t* viewleaf, const reference_mode_t* ref_mode, c
 
 	ubo->time = fd->time;
 	ubo->num_static_primitives = (vkpt_refdef.bsp_mesh_world.world_idx_count + vkpt_refdef.bsp_mesh_world.world_transparent_count) / 3;
+	ubo->num_static_lights = vkpt_refdef.bsp_mesh_world.num_light_polys;
 
 #define UBO_CVAR_DO(name, default_value) ubo->name = cvar_##name->value;
 	UBO_CVAR_LIST
@@ -2819,6 +2823,7 @@ R_BeginRegistration_RTX(const char *name)
 	bsp_world_model = bsp;
 	bsp_mesh_register_textures(bsp);
 	bsp_mesh_create_from_bsp(&vkpt_refdef.bsp_mesh_world, bsp, name);
+	vkpt_light_stats_create(&vkpt_refdef.bsp_mesh_world);
 	_VK(vkpt_vertex_buffer_upload_bsp_mesh_to_staging(&vkpt_refdef.bsp_mesh_world));
 	_VK(vkpt_vertex_buffer_upload_staging());
 	vkpt_refdef.bsp_mesh_world_loaded = 1;

src/refresh/vkpt/vertex_buffer.c
@@ -53,6 +53,10 @@ vkpt_vertex_buffer_upload_staging()
 		.size = VK_WHOLE_SIZE,
 	);
 
+	vkCmdFillBuffer(cmd_buf, qvk.buf_light_stats[0].buffer, 0, qvk.buf_light_stats[0].size, 0);
+	vkCmdFillBuffer(cmd_buf, qvk.buf_light_stats[1].buffer, 0, qvk.buf_light_stats[1].size, 0);
+	vkCmdFillBuffer(cmd_buf, qvk.buf_light_stats[2].buffer, 0, qvk.buf_light_stats[2].size, 0);
+
 	vkpt_submit_command_buffer(cmd_buf, qvk.queue_graphics, (1 << qvk.device_count) - 1, 0, NULL, NULL, NULL, 0, NULL, NULL, qvk.fence_vertex_sync);
 
 	return VK_SUCCESS;
@@ -69,6 +73,9 @@ vkpt_light_buffer_upload_staging(VkCommandBuffer cmd_buf)
 		.size = sizeof(LightBuffer),
 	};
 	vkCmdCopyBuffer(cmd_buf, staging->buffer, qvk.buf_light.buffer, 1, &copyRegion);
+
+	int buffer_idx = qvk.frame_counter % 3;
+	vkCmdFillBuffer(cmd_buf, qvk.buf_light_stats[buffer_idx].buffer, 0, qvk.buf_light_stats[buffer_idx].size, 0);
 
 	return VK_SUCCESS;
 }
@@ -493,6 +500,12 @@ vkpt_vertex_buffer_create()
 			.descriptorCount = 1,
 			.binding = SUN_COLOR_UBO_BINDING_IDX,
 			.stageFlags = VK_SHADER_STAGE_ALL,
+		},
+		{
+			.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER,
+			.descriptorCount = 3,
+			.binding = LIGHT_STATS_BUFFER_BINDING_IDX,
+			.stageFlags = VK_SHADER_STAGE_ALL,
 		}
 	};
 
@@ -611,6 +624,7 @@ vkpt_vertex_buffer_create()
 	buf_info.range = sizeof(SunColorBuffer);
 	vkUpdateDescriptorSets(qvk.device, 1, &output_buf_write, 0, NULL);
 
+
 	return VK_SUCCESS;
 }
 
@@ -651,6 +665,61 @@ vkpt_vertex_buffer_destroy()
 
 	buffer_destroy(&qvk.buf_tonemap);
 	buffer_destroy(&qvk.buf_sun_color);
+
+	return VK_SUCCESS;
+}
+
+VkResult vkpt_light_stats_create(bsp_mesh_t *bsp_mesh)
+{
+	vkpt_light_stats_destroy();
+
+	// Light statistics: 2 uints (shadowed, unshadowed) per light per surface orientation (6) per cluster.
+	uint32_t num_stats = bsp_mesh->num_clusters * bsp_mesh->num_light_polys * 6 * 2;
+
+	for (int frame = 0; frame < NUM_LIGHT_STATS_BUFFERS; frame++)
+	{
+		buffer_create(qvk.buf_light_stats + frame, sizeof(uint32_t) * num_stats,
+			VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_STORAGE_BUFFER_BIT,
+			VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+	}
+
+	assert(NUM_LIGHT_STATS_BUFFERS == 3);
+
+	VkDescriptorBufferInfo light_stats_buf_info[] = { {
+			.buffer = qvk.buf_light_stats[0].buffer,
+			.offset = 0,
+			.range = qvk.buf_light_stats[0].size,
+		}, {
+			.buffer = qvk.buf_light_stats[1].buffer,
+			.offset = 0,
+			.range = qvk.buf_light_stats[1].size,
+		}, {
+			.buffer = qvk.buf_light_stats[2].buffer,
+			.offset = 0,
+			.range = qvk.buf_light_stats[2].size,
+		} };
+
+	VkWriteDescriptorSet output_buf_write = {
+		.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET,
+		.dstSet = qvk.desc_set_vertex_buffer,
+		.dstBinding = 6,
+		.dstArrayElement = 0,
+		.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER,
+		.descriptorCount = LENGTH(light_stats_buf_info),
+		.pBufferInfo = light_stats_buf_info,
+	};
+
+	vkUpdateDescriptorSets(qvk.device, 1, &output_buf_write, 0, NULL);
+
+	return VK_SUCCESS;
+}
+
+VkResult vkpt_light_stats_destroy()
+{
+	for (int frame = 0; frame < NUM_LIGHT_STATS_BUFFERS; frame++)
+	{
+		buffer_destroy(qvk.buf_light_stats + frame);
+	}
 
 	return VK_SUCCESS;
 }

src/refresh/vkpt/shader/constants.h
@@ -42,6 +42,8 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #define NUM_BLUE_NOISE_TEX (128 * 4)
 #define BLUE_NOISE_RES     (256)
 
+#define NUM_LIGHT_STATS_BUFFERS 3
+
 #define MATERIAL_KIND_MASK           0xf0000000
 #define MATERIAL_KIND_INVALID        0x00000000
 #define MATERIAL_KIND_REGULAR        0x10000000

src/refresh/vkpt/shader/global_ubo.h
@@ -73,6 +73,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 	UBO_CVAR_DO(pt_fake_roughness_threshold, 0.20) /* roughness value where the path tracer starts switching indirect light specular sampling from NDF based to SH based, [0..1] */ \
 	UBO_CVAR_DO(pt_indirect_polygon_lights, 1) /* switch for bounce lighting from local polygon lights, 0 or 1 */ \
 	UBO_CVAR_DO(pt_indirect_sphere_lights, 1) /* switch for bounce lighting from local sphere lights, 0 or 1 */ \
+	UBO_CVAR_DO(pt_light_stats, 1) /* switch for statistical light PDF correction, 0 or 1 */ \
 	UBO_CVAR_DO(pt_max_log_sky_luminance, -3) /* maximum sky luminance, log2 scale, used for polygon light selection, (-inf..inf) */ \
 	UBO_CVAR_DO(pt_metallic_override, -1) /* overrides metallic parameter of all materials if non-negative, [0..1] */ \
 	UBO_CVAR_DO(pt_ndf_trim, 0.8) /* trim factor for GGX NDF sampling (0..1] */ \
@@ -140,15 +141,20 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 	GLOBAL_UBO_VAR_LIST_DO(int,             planet_albedo_map) \
 	GLOBAL_UBO_VAR_LIST_DO(int,             planet_normal_map) \
     \
-	GLOBAL_UBO_VAR_LIST_DO(int,             num_lights) \
+	GLOBAL_UBO_VAR_LIST_DO(int,             num_sphere_lights) \
+	GLOBAL_UBO_VAR_LIST_DO(int ,            num_static_lights) \
 	GLOBAL_UBO_VAR_LIST_DO(int,             num_static_primitives) \
 	GLOBAL_UBO_VAR_LIST_DO(int,             cluster_debug_index) \
-	GLOBAL_UBO_VAR_LIST_DO(int,             water_normal_texture) \
 	\
+	GLOBAL_UBO_VAR_LIST_DO(int,             water_normal_texture) \
 	GLOBAL_UBO_VAR_LIST_DO(float,           pt_env_scale) \
 	GLOBAL_UBO_VAR_LIST_DO(float,           cylindrical_hfov) \
 	GLOBAL_UBO_VAR_LIST_DO(float,           cylindrical_hfov_prev) \
+	\
 	GLOBAL_UBO_VAR_LIST_DO(int,             pt_swap_checkerboard) \
+	GLOBAL_UBO_VAR_LIST_DO(int,             padding1) \
+	GLOBAL_UBO_VAR_LIST_DO(int,             padding2) \
+	GLOBAL_UBO_VAR_LIST_DO(int,             padding3) \
 	\
 	GLOBAL_UBO_VAR_LIST_DO(vec4,            dynamic_light_data[MAX_LIGHT_SOURCES * 2]) \
 	GLOBAL_UBO_VAR_LIST_DO(vec4,            cam_pos) \

src/refresh/vkpt/shader/light_lists.h
@@ -79,6 +79,15 @@ sample_projected_triangle(vec3 p, mat3 positions, vec2 rnd, out vec3 light_norma
 	return p + lo;
 }
 
+uint get_light_stats_addr(uint cluster, uint light, uint side)
+{
+	uint addr = cluster;
+	addr = addr * global_ubo.num_static_lights + light;
+	addr = addr * 6 + side;
+	addr = addr * 2;
+	return addr;
+}
+
 void
 sample_light_list(
 		uint list_idx,
@@ -93,8 +102,11 @@ sample_light_list(
 		out vec3 light_color,
 		out vec3 light_normal,
 		out float pdf,
+		out int light_index,
 		vec3 rng)
 {
+	light_index = -1;
+
 	if(list_idx == ~0u)
 	{
 		position_light = vec3(0);
@@ -145,6 +157,33 @@ sample_light_list(
 		else
 			m *= abs(light_lum); // abs because sky lights have negative color
 
+		// Apply CDF adjustment based on light shadowing statistics from one of the previous frames.
+		// See comments in function `get_direct_illumination` in `path_tracer_rgen.h`
+		if(global_ubo.pt_light_stats != 0 
+			&& m > 0 
+			&& current_idx < global_ubo.num_static_lights)
+		{
+			uint buffer_idx = global_ubo.current_frame_idx;
+			// Regular pixels get shadowing stats from the previous frame;
+			// Gradient pixels get the stats from two frames ago because they need to match
+			// the light sampling from the previous frame.
+			buffer_idx += is_gradient ? (NUM_LIGHT_STATS_BUFFERS - 2) : (NUM_LIGHT_STATS_BUFFERS - 1);
+			buffer_idx = buffer_idx % NUM_LIGHT_STATS_BUFFERS;
+
+			uint addr = get_light_stats_addr(list_idx, current_idx, get_primary_direction(n));
+
+			uint num_hits = light_stats_bufers[buffer_idx].stats[addr];
+			uint num_misses = light_stats_bufers[buffer_idx].stats[addr + 1];
+			uint num_total = num_hits + num_misses;
+
+			if(num_total > 0)
+			{
+				// Adjust the mass, but set a lower limit on the factor to avoid
+				// extreme changes in the sampling.
+				m *= max(float(num_hits) / float(num_total), 0.1);
+			}
+		}
+
 		mass += m;
 		light_masses[i] = m;
 	}
@@ -194,6 +233,8 @@ sample_light_list(
 			light_color = light.color * (area * spotlight * light.light_style_scale);
 		else
 			light_color = env_map(L, true) * area * global_ubo.pt_env_scale;
+
+		light_index = current_idx;
 	}
 }
 

src/refresh/vkpt/shader/path_tracer_rgen.h
@@ -350,7 +350,7 @@ AdjustRoughnessToksvig(float roughness, float normalMapLen, float mip_level)
 }
 
 vec3
-compute_direct_illumination_static(vec3 position, vec3 normal, vec3 geo_normal, vec3 view_direction, float phong_exp, float phong_weight, int bounce, uint cluster, bool is_gradient, out vec3 pos_on_light)
+compute_direct_illumination_static(vec3 position, vec3 normal, vec3 geo_normal, vec3 view_direction, float phong_exp, float phong_weight, int bounce, uint cluster, bool is_gradient, out vec3 pos_on_light, out int light_index)
 {
 	float pdf;
 	vec3 light_color;
@@ -369,6 +369,7 @@ compute_direct_illumination_static(vec3 position, vec3 normal, vec3 geo_normal, 
 			light_color,
 			light_normal,
 			pdf,
+			light_index,
 			vec3(
 				get_rng(RNG_NEE_LH(bounce)),
 				get_rng(RNG_NEE_TRI_X(bounce)),
@@ -384,11 +385,11 @@ compute_direct_illumination_static(vec3 position, vec3 normal, vec3 geo_normal, 
 vec3
 compute_direct_illumination_dynamic(vec3 position, vec3 normal, vec3 geo_normal, uint bounce, out vec3 pos_on_light)
 {
-	if(global_ubo.num_lights == 0)
+	if(global_ubo.num_sphere_lights == 0)
 		return vec3(0);
 
-	float random_light = get_rng(RNG_NEE_LH(bounce)) * global_ubo.num_lights;
-	uint light_idx = min(global_ubo.num_lights - 1, uint(random_light));
+	float random_light = get_rng(RNG_NEE_LH(bounce)) * global_ubo.num_sphere_lights;
+	uint light_idx = min(global_ubo.num_sphere_lights - 1, uint(random_light));
 
 	// Limit the solid angle of sphere lights for indirect lighting 
 	// in order to kill some fireflies in locations with many sphere lights.
@@ -408,7 +409,7 @@ compute_direct_illumination_dynamic(vec3 position, vec3 normal, vec3 geo_normal,
 
     light_color *= DYNAMIC_LIGHT_INTENSITY_DIFFUSE;
 
-    return light_color * float(global_ubo.num_lights);
+    return light_color * float(global_ubo.num_sphere_lights);
 }
 
 void
@@ -444,9 +445,11 @@ get_direct_illumination(
 	float phong_exp = RoughnessToSpecPower(roughness);
 	float phong_weight = surface_specular * direct_specular_weight;
 
+	int static_light_index = -1;
+
 	/* static illumination */
 	if(enable_static) {
-		contrib_static = compute_direct_illumination_static(position, normal, geo_normal, view_direction, phong_exp, phong_weight, bounce, cluster_idx, is_gradient, pos_on_light_static);
+		contrib_static = compute_direct_illumination_static(position, normal, geo_normal, view_direction, phong_exp, phong_weight, bounce, cluster_idx, is_gradient, pos_on_light_static, static_light_index);
 	}
 
 	bool is_static = true;
@@ -482,6 +485,38 @@ get_direct_illumination(
 		contrib *= trace_caustic_ray(shadow_ray, surface_medium);
 	}
 #endif
+
+	/* 
+		Accumulate light shadowing statistics to guide importance sampling on the next frame.
+		Inspired by paper called "Adaptive Shadow Testing for Ray Tracing" by G. Ward, EUROGRAPHICS 1994.
+
+		The algorithm counts the shadowed and unshadowed rays towards each light, per cluster,
+		per surface orientation in each cluster. Orientation helps improve accuracy in cases 
+		when a single cluster has different parts which have the same light mostly shadowed and 
+		mostly unshadowed.
+
+		On the next frame, the light CDF is built using the counts from this frame, or the frame
+		before that in case of gradient rays. See light_lists.h for more info.
+
+		Only applies to static polygon lights (i.e. no model or beam lights) because the dynamic
+		polygon lights do not have static indices, and it would be difficult to map them 
+		between frames.
+	*/
+	if(global_ubo.pt_light_stats != 0 
+		&& is_static 
+		&& !null_light
+		&& static_light_index >= 0 
+		&& static_light_index < global_ubo.num_static_lights)
+	{
+		uint addr = get_light_stats_addr(cluster_idx, static_light_index, get_primary_direction(normal));
+
+		// Offset 0 is unshadowed rays,
+		// Offset 1 is shadowed rays
+		if(vis == 0) addr += 1;
+
+		// Increment the ray counter
+		atomicAdd(light_stats_bufers[global_ubo.current_frame_idx % NUM_LIGHT_STATS_BUFFERS].stats[addr], 1);
+	}
 
 	if(null_light)
 		return;

src/refresh/vkpt/shader/utils.glsl
@@ -480,4 +480,15 @@ vec3 unpackRGBE(uint x)
     return v;
 }
 
+uint get_primary_direction(vec3 dir)
+{
+    vec3 adir = abs(dir);
+    if(adir.x > adir.y && adir.x > adir.z)
+        return (dir.x < 0) ? 1 : 0;
+    if(adir.y > adir.z)
+        return (dir.y < 0) ? 3 : 2;
+    return (dir.z < 0) ? 5 : 4;
+}
+
+
 #endif /*_GLSL_UTILS_GLSL*/

src/refresh/vkpt/shader/vertex_buffer.h
@@ -48,6 +48,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #define TONE_MAPPING_BUFFER_BINDING_IDX 3
 #define SUN_COLOR_BUFFER_BINDING_IDX 4
 #define SUN_COLOR_UBO_BINDING_IDX 5
+#define LIGHT_STATS_BUFFER_BINDING_IDX 6
 
 #define SUN_COLOR_ACCUMULATOR_FIXED_POINT_SCALE 0x100000
 #define SKY_COLOR_ACCUMULATOR_FIXED_POINT_SCALE 0x1000
@@ -212,6 +213,11 @@ layout(set = VERTEX_BUFFER_DESC_SET_IDX, binding = SUN_COLOR_BUFFER_BINDING_IDX)
 layout(set = VERTEX_BUFFER_DESC_SET_IDX, binding = SUN_COLOR_UBO_BINDING_IDX, std140) uniform SUN_COLOR_UBO {
 	SunColorBuffer sun_color_ubo;
 };
+
+layout(set = VERTEX_BUFFER_DESC_SET_IDX, binding = LIGHT_STATS_BUFFER_BINDING_IDX) buffer LIGHT_STATS_BUFFERS {
+	uint stats[];
+} light_stats_bufers[3];
+
 
 #define GET_float_1(buf,name) \
 float \

commit c1774b3a3aab43342c04c56b6fb7765578569621

Fixed crashes on vid_restart and reload_shader that were introduced in https://github.com/NVIDIA/Q2RTX/commit/89387a27b39f1849be46671e89560c9939c402b5

src/refresh/vkpt/main.c
@@ -209,9 +209,8 @@ vkpt_destroy_all(VkptInitFlags_t destroy_flags)
 	if ((VKPT_INIT_DEFAULT & destroy_flags) == destroy_flags)
 	{
 		destroy_transparency();
+		vkpt_light_stats_destroy();
 	}
-
-	vkpt_light_stats_destroy();
 
 	return VK_SUCCESS;
 }

src/refresh/vkpt/vertex_buffer.c
@@ -53,9 +53,12 @@ vkpt_vertex_buffer_upload_staging()
 		.size = VK_WHOLE_SIZE,
 	);
 
-	vkCmdFillBuffer(cmd_buf, qvk.buf_light_stats[0].buffer, 0, qvk.buf_light_stats[0].size, 0);
-	vkCmdFillBuffer(cmd_buf, qvk.buf_light_stats[1].buffer, 0, qvk.buf_light_stats[1].size, 0);
-	vkCmdFillBuffer(cmd_buf, qvk.buf_light_stats[2].buffer, 0, qvk.buf_light_stats[2].size, 0);
+	if (qvk.buf_light_stats[0].buffer)
+	{
+		vkCmdFillBuffer(cmd_buf, qvk.buf_light_stats[0].buffer, 0, qvk.buf_light_stats[0].size, 0);
+		vkCmdFillBuffer(cmd_buf, qvk.buf_light_stats[1].buffer, 0, qvk.buf_light_stats[1].size, 0);
+		vkCmdFillBuffer(cmd_buf, qvk.buf_light_stats[2].buffer, 0, qvk.buf_light_stats[2].size, 0);
+	}
 
 	vkpt_submit_command_buffer(cmd_buf, qvk.queue_graphics, (1 << qvk.device_count) - 1, 0, NULL, NULL, NULL, 0, NULL, NULL, qvk.fence_vertex_sync);
 
@@ -75,7 +78,10 @@ vkpt_light_buffer_upload_staging(VkCommandBuffer cmd_buf)
 	vkCmdCopyBuffer(cmd_buf, staging->buffer, qvk.buf_light.buffer, 1, &copyRegion);
 
 	int buffer_idx = qvk.frame_counter % 3;
-	vkCmdFillBuffer(cmd_buf, qvk.buf_light_stats[buffer_idx].buffer, 0, qvk.buf_light_stats[buffer_idx].size, 0);
+	if (qvk.buf_light_stats[buffer_idx].buffer)
+	{
+		vkCmdFillBuffer(cmd_buf, qvk.buf_light_stats[buffer_idx].buffer, 0, qvk.buf_light_stats[buffer_idx].size, 0);
+	}
 
 	return VK_SUCCESS;
 }