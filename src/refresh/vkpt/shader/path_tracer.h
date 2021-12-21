cynicgamesofficial/Quasimodo-RTX

commit b4032ef22822dfb0e318731ea53d31d6ed719e96

Reduced the effects payload to get back the performance lost in ray pipeline mode, and because the split near/far tracking is no longer necessary.

src/refresh/vkpt/shader/path_tracer.h
@@ -153,15 +153,8 @@ struct RayPayloadGeometry {
 };
 
 struct RayPayloadEffects {
-   uvec2 close_transparencies; // half4x16
-   uvec2 farthest_transparency; // half4x16
-   float closest_max_transparent_distance;
-   float farthest_transparent_distance;
-   float farthest_transparent_depth;
-};
-
-struct RayPayloadShadow {
-	int missed;
+   uvec2 transparency; // half4x16
+   uint distances; // half2x16 - min and max
 };
 
 struct HitAttributeBeam {

src/refresh/vkpt/shader/path_tracer_beam.rahit
@@ -39,11 +39,11 @@ void main()
 {
 	const vec2 beam_fade_and_thickness = unpackHalf2x16(beam_hit_attrib.fade_and_thickness);
 
-	transparency_result_t transparency = pt_logic_beam(gl_PrimitiveID, beam_fade_and_thickness);
+	vec4 transparency = pt_logic_beam(gl_PrimitiveID, beam_fade_and_thickness, gl_HitTEXT, gl_RayTmaxEXT);
 	
-	if (transparency.color.a > 0)
+	if (transparency.a > 0)
 	{
-		update_payload_transparency(ray_payload, transparency.color, transparency.thickness, gl_HitTEXT);
+		update_payload_transparency(ray_payload, transparency, gl_HitTEXT);
 	}
 
 	ignoreIntersectionEXT;

src/refresh/vkpt/shader/path_tracer_rgen.h
@@ -318,11 +318,8 @@ trace_effects_ray(Ray ray, bool skip_procedural)
 	
 	uint instance_mask = AS_FLAG_EFFECTS;
 
-	ray_payload_effects.close_transparencies = uvec2(0);
-	ray_payload_effects.farthest_transparency = uvec2(0);
-    ray_payload_effects.closest_max_transparent_distance = 0;
-	ray_payload_effects.farthest_transparent_distance = 0;
-	ray_payload_effects.farthest_transparent_depth = 0;
+	ray_payload_effects.transparency = uvec2(0);
+	ray_payload_effects.distances = 0;
 
 #ifdef KHR_RAY_QUERY
 
@@ -340,9 +337,7 @@ trace_effects_ray(Ray ray, bool skip_procedural)
 		vec2 bary = rayQueryGetIntersectionBarycentricsEXT(rayQuery, false);
 		bool isProcedural = rayQueryGetIntersectionTypeEXT(rayQuery, false) == gl_RayQueryCandidateIntersectionAABBEXT;
 
-		transparency_result_t transparency;
-		transparency.color = vec4(0);
-		transparency.thickness = 0;
+		vec4 transparent = vec4(0);
 
 		if (isProcedural)
 		{
@@ -360,7 +355,7 @@ trace_effects_ray(Ray ray, bool skip_procedural)
 				// Then the any-hit shader.
 				if (intersectsWithBeam)
 				{
-					transparency = pt_logic_beam(primitiveID, beam_fade_and_thickness);
+					transparent = pt_logic_beam(primitiveID, beam_fade_and_thickness, tShapeHit, ray.t_max);
 					hitT = tShapeHit;
 				}
 			}
@@ -370,22 +365,22 @@ trace_effects_ray(Ray ray, bool skip_procedural)
 			switch(sbtOffset)
 			{
 			case SBTO_PARTICLE: // particles
-				transparency = pt_logic_particle(primitiveID, bary);
+				transparent = pt_logic_particle(primitiveID, bary);
 				break;
 
 			case SBTO_EXPLOSION: // explosions
-				transparency = pt_logic_explosion(primitiveID, instanceCustomIndex, ray.direction, bary);
+				transparent = pt_logic_explosion(primitiveID, instanceCustomIndex, ray.direction, bary);
 				break;
 
 			case SBTO_SPRITE: // sprites
-				transparency = pt_logic_sprite(primitiveID, bary);
+				transparent = pt_logic_sprite(primitiveID, bary);
 				break;
 			}
 		}
 
-		if (transparency.color.a > 0)
+		if (transparent.a > 0)
 		{
-			update_payload_transparency(ray_payload_effects, transparency.color, transparency.thickness, hitT);
+			update_payload_transparency(ray_payload_effects, transparent, hitT);
 		}
 	}
 
@@ -397,7 +392,7 @@ trace_effects_ray(Ray ray, bool skip_procedural)
 
 #endif
 
-	return get_payload_transparency(ray_payload_effects, ray.t_max);
+	return get_payload_transparency(ray_payload_effects);
 }
 
 Ray get_shadow_ray(vec3 p1, vec3 p2, float tmin)

src/refresh/vkpt/shader/path_tracer_explosion.rahit
@@ -37,11 +37,11 @@ hitAttributeEXT vec2 hit_attribs;
 
 void main()
 {
-	transparency_result_t transparency = pt_logic_explosion(gl_PrimitiveID, gl_InstanceCustomIndexEXT, gl_WorldRayDirectionEXT, hit_attribs.xy);
+	vec4 transparency = pt_logic_explosion(gl_PrimitiveID, gl_InstanceCustomIndexEXT, gl_WorldRayDirectionEXT, hit_attribs.xy);
 
-	if (transparency.color.a > 0)
+	if (transparency.a > 0)
 	{
-		update_payload_transparency(ray_payload, transparency.color, transparency.thickness, gl_HitTEXT);
+		update_payload_transparency(ray_payload, transparency, gl_HitTEXT);
 	}
 
 	ignoreIntersectionEXT;

src/refresh/vkpt/shader/path_tracer_hit_shaders.h
@@ -67,22 +67,12 @@ bool pt_logic_masked(int primitiveID, uint instanceCustomIndex, vec2 bary)
 	return mask_value.x >= 0.5;
 }
 
-struct transparency_result_t
-{
-	vec4 color;
-	float thickness;
-};
-
-transparency_result_t pt_logic_particle(int primitiveID, vec2 bary)
+vec4 pt_logic_particle(int primitiveID, vec2 bary)
 {
 	const vec3 barycentric = vec3(1.0 - bary.x - bary.y, bary.x, bary.y);
 	const vec2 uv = vec2(0.0, 0.0) * barycentric.x + vec2(1.0, 0.0) * barycentric.y + vec2(1.0, 1.0) * barycentric.z;
 
 	const float factor = pow(clamp(1.0 - length(vec2(0.5) - uv) * 2.0, 0.0, 1.0), global_ubo.pt_particle_softness);
-
-	transparency_result_t result;
-	result.color = vec4(0);
-	result.thickness = 0;
 
 	if (factor > 0.0)
 	{
@@ -93,20 +83,16 @@ transparency_result_t pt_logic_particle(int primitiveID, vec2 bary)
 
 		color.rgb *= global_ubo.prev_adapted_luminance * 500;
 
-		result.color = color;
+		return color;
 	}
 
-	return result;
+	return vec4(0);
 }
 
-transparency_result_t pt_logic_beam(int primitiveID, vec2 beam_fade_and_thickness)
+vec4 pt_logic_beam(int primitiveID, vec2 beam_fade_and_thickness, float hitT, float maxT)
 {
 	const float x = beam_fade_and_thickness.x;
 	const float factor = pow(clamp(x, 0.0, 1.0), global_ubo.pt_beam_softness);
-
-	transparency_result_t result;
-	result.color = vec4(0);
-	result.thickness = 0;
 
 	if (factor > 0.0)
 	{
@@ -123,14 +109,20 @@ transparency_result_t pt_logic_beam(int primitiveID, vec2 beam_fade_and_thicknes
 	    float noise = texelFetch(TEX_BLUE_NOISE, ivec3(texpos, texnum), 0).r;
 	    color.rgb *= noise * noise + 0.1;
 
-	    result.color = color;
-	    result.thickness = beam_fade_and_thickness.y;
+	    float thickness = beam_fade_and_thickness.y;
+	    if (thickness > 0)
+	    {
+	    	float ratio = clamp((maxT - hitT) / thickness, 0.0, 1.0);
+	    	color.rgba *= ratio;
+	    }
+
+	    return color;
 	}
 
-	return result;
+	return vec4(0);
 }
 
-transparency_result_t pt_logic_sprite(int primitiveID, vec2 bary)
+vec4 pt_logic_sprite(int primitiveID, vec2 bary)
 {
 	const vec3 barycentric = vec3(1.0 - bary.x - bary.y, bary.x, bary.y);
 	
@@ -158,14 +150,10 @@ transparency_result_t pt_logic_sprite(int primitiveID, vec2 bary)
 		color.rgb *= global_ubo.prev_adapted_luminance * 2000;
 	}
 
-	transparency_result_t result;
-	result.color = color;
-	result.thickness = 0;
-
-	return result;
+	return color;
 }
 
-transparency_result_t pt_logic_explosion(int primitiveID, uint instanceCustomIndex, vec3 worldRayDirection, vec2 bary)
+vec4 pt_logic_explosion(int primitiveID, uint instanceCustomIndex, vec3 worldRayDirection, vec2 bary)
 {
 	const uint primitive_id = primitiveID + instanceCustomIndex & AS_INSTANCE_MASK_OFFSET;
 	const Triangle triangle = get_instanced_triangle(primitive_id);
@@ -188,11 +176,7 @@ transparency_result_t pt_logic_explosion(int primitiveID, uint instanceCustomInd
 
 	emission.rgb *= global_ubo.prev_adapted_luminance * 500;
 
-	transparency_result_t result;
-	result.color = emission;
-	result.thickness = 0;
-
-	return result;
+	return emission;
 }
 
 // Adapted from: http://www.pbr-book.org/3ed-2018/Utilities/Mathematical_Routines.html#SolvingQuadraticEquations

src/refresh/vkpt/shader/path_tracer_particle.rahit
@@ -37,11 +37,11 @@ hitAttributeEXT vec2 hit_attribs;
 
 void main()
 {
-	transparency_result_t transparency = pt_logic_particle(gl_PrimitiveID, hit_attribs.xy);
+	vec4 transparency = pt_logic_particle(gl_PrimitiveID, hit_attribs.xy);
 
-	if (transparency.color.a > 0)
+	if (transparency.a > 0)
 	{
-		update_payload_transparency(ray_payload, transparency.color, transparency.thickness, gl_HitTEXT);
+		update_payload_transparency(ray_payload, transparency, gl_HitTEXT);
 	}
 
 	ignoreIntersectionEXT;

src/refresh/vkpt/shader/path_tracer_sprite.rahit
@@ -37,11 +37,11 @@ hitAttributeEXT vec2 hit_attribs;
 
 void main()
 {
-	transparency_result_t transparency = pt_logic_sprite(gl_PrimitiveID, hit_attribs.xy);
+	vec4 transparency = pt_logic_sprite(gl_PrimitiveID, hit_attribs.xy);
 	
-	if (transparency.color.a > 0)
+	if (transparency.a > 0)
 	{
-		update_payload_transparency(ray_payload, transparency.color, transparency.thickness, gl_HitTEXT);
+		update_payload_transparency(ray_payload, transparency, gl_HitTEXT);
 	}
 
 	ignoreIntersectionEXT;

src/refresh/vkpt/shader/path_tracer_transparency.glsl
@@ -20,39 +20,30 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #ifndef PATH_TRACER_TRANSPARENCY_GLSL_
 #define PATH_TRACER_TRANSPARENCY_GLSL_
 
-void update_payload_transparency(inout RayPayloadEffects rp, vec4 color, float thickness, float hitT)
+void update_payload_transparency(inout RayPayloadEffects rp, vec4 color, float hitT)
 {
-	if(hitT > rp.farthest_transparent_distance)
+	vec4 accumulated_color = unpackHalf4x16(rp.transparency);
+	vec2 distances = unpackHalf2x16(rp.distances);
+
+	if(hitT < distances.x || distances.x == 0)
 	{
-		rp.close_transparencies = packHalf4x16(alpha_blend_premultiplied(unpackHalf4x16(rp.close_transparencies), unpackHalf4x16(rp.farthest_transparency)));
-		rp.closest_max_transparent_distance = rp.farthest_transparent_distance;
-		rp.farthest_transparency = packHalf4x16(color);
-		rp.farthest_transparent_distance = hitT;
-		rp.farthest_transparent_depth = thickness;
-	}
-	else if(rp.closest_max_transparent_distance < hitT)
-	{
-		rp.close_transparencies = packHalf4x16(alpha_blend_premultiplied(unpackHalf4x16(rp.close_transparencies), color));
-		rp.closest_max_transparent_distance = hitT;
+		accumulated_color = alpha_blend_premultiplied(color, accumulated_color);
+		distances.x = hitT;
 	}
 	else
-		rp.close_transparencies = packHalf4x16(alpha_blend_premultiplied(color, unpackHalf4x16(rp.close_transparencies)));
-}
-
-vec4 get_payload_transparency(in RayPayloadEffects rp, float solidDist)
-{
-	float scale_far = 1;
-	if (rp.farthest_transparent_depth > 0)
 	{
-		scale_far = clamp((solidDist - rp.farthest_transparent_distance) / rp.farthest_transparent_depth, 0, 1);
+		accumulated_color = alpha_blend_premultiplied(accumulated_color, color);
 	}
+	
+	distances.y = max(distances.y, hitT);
 
-	return alpha_blend_premultiplied(unpackHalf4x16(rp.close_transparencies), unpackHalf4x16(rp.farthest_transparency) * scale_far);
+	rp.transparency = packHalf4x16(accumulated_color);
+	rp.distances = packHalf2x16(distances);
 }
 
-vec4 get_payload_transparency_simple(in RayPayloadEffects rp)
+vec4 get_payload_transparency(in RayPayloadEffects rp)
 {
-	return alpha_blend_premultiplied(unpackHalf4x16(rp.close_transparencies), unpackHalf4x16(rp.farthest_transparency));
+	return unpackHalf4x16(rp.transparency);
 }
 
 #endif // PATH_TRACER_TRANSPARENCY_GLSL_

commit 9a6e0dabb23000adc1385ae050c1a579650fd03e

Fixed the rendering of beams in ray pipeline mode. It was broken since b4032ef22822dfb0e318731ea53d31d6ed719e96.

src/refresh/vkpt/shader/path_tracer.h
@@ -157,6 +157,12 @@ struct RayPayloadEffects {
    uint distances; // half2x16 - min and max
    uvec4 fog1; // half8x16: .xy = color.rgba; .z = t_min, t_max; .w = density: a and b for (a*t + b)
    uvec4 fog2; // same as fog1 but for a fog volume further away
+#ifndef KHR_RAY_QUERY
+   // Store TMax in the payload because gl_RayTmaxEXT changes while the ray is being traced.
+   // See the GLSL_EXT_ray_tracing spec near "description for gl_RayTminEXT and gl_RayTmaxEXT"
+   // https://github.com/KhronosGroup/GLSL/blob/master/extensions/ext/GLSL_EXT_ray_tracing.txt 
+   float rayTmax; 
+#endif
 };
 
 struct HitAttributeBeam {

src/refresh/vkpt/shader/path_tracer_beam.rahit
@@ -39,7 +39,7 @@ void main()
 {
 	const vec2 beam_fade_and_thickness = unpackHalf2x16(beam_hit_attrib.fade_and_thickness);
 
-	vec4 transparency = pt_logic_beam(gl_PrimitiveID, beam_fade_and_thickness, gl_HitTEXT, gl_RayTmaxEXT);
+	vec4 transparency = pt_logic_beam(gl_PrimitiveID, beam_fade_and_thickness, gl_HitTEXT, ray_payload.rayTmax);
 	
 	if (transparency.a > 0)
 	{

src/refresh/vkpt/shader/path_tracer_rgen.h
@@ -373,6 +373,9 @@ trace_effects_ray(Ray ray, bool skip_procedural)
 	ray_payload_effects.distances = 0;
 	ray_payload_effects.fog1 = uvec4(0);
 	ray_payload_effects.fog2 = uvec4(0);
+#ifndef KHR_RAY_QUERY
+	ray_payload_effects.rayTmax = ray.t_max;
+#endif
 
 	if (!skip_procedural)
 		find_fog_volumes(ray_payload_effects, ray);