skullernet/q2pro

Const-ify vector input parameters to some renderer API and internal functions.

src/refresh/gl.h
@@ -220,9 +220,9 @@ typedef enum {
     CULL_CLIP
 } glCullResult_t;
 
-glCullResult_t GL_CullBox(vec3_t bounds[2]);
+glCullResult_t GL_CullBox(const vec3_t bounds[2]);
 glCullResult_t GL_CullSphere(const vec3_t origin, float radius);
-glCullResult_t GL_CullLocalBox(const vec3_t origin, vec3_t bounds[2]);
+glCullResult_t GL_CullLocalBox(const vec3_t origin, const vec3_t bounds[2]);
 
 bool GL_AllocBlock(int width, int height, int *inuse,
                    int w, int h, int *s, int *t);
@@ -584,7 +584,7 @@ void GL_ClearSolidFaces(void);
 void GL_DrawBspModel(mmodel_t *model);
 void GL_DrawWorld(void);
 void GL_SampleLightPoint(vec3_t color);
-void GL_LightPoint(vec3_t origin, vec3_t color);
+void GL_LightPoint(const vec3_t origin, vec3_t color);
 
 /*
  * gl_sky.c
@@ -593,7 +593,7 @@ void GL_LightPoint(vec3_t origin, vec3_t color);
 void R_AddSkySurface(mface_t *surf);
 void R_ClearSkyBox(void);
 void R_DrawSkyBox(void);
-void R_SetSky(const char *name, float rotate, vec3_t axis);
+void R_SetSky(const char *name, float rotate, const vec3_t axis);
 
 /*
  * gl_mesh.c

src/refresh/sky.c
@@ -379,7 +379,7 @@ static void R_UnsetSky(void)
 R_SetSky
 ============
 */
-void R_SetSky(const char *name, float rotate, vec3_t axis)
+void R_SetSky(const char *name, float rotate, const vec3_t axis)
 {
     int     i;
     char    pathname[MAX_QPATH];

src/refresh/world.c
@@ -72,7 +72,7 @@ void GL_SampleLightPoint(vec3_t color)
     }
 }
 
-static bool _GL_LightPoint(vec3_t start, vec3_t color)
+static bool _GL_LightPoint(const vec3_t start, vec3_t color)
 {
     bsp_t           *bsp;
     int             i, index;
@@ -210,7 +210,7 @@ static void GL_TransformLights(mmodel_t *model)
     }
 }
 
-static void GL_AddLights(vec3_t origin, vec3_t color)
+static void GL_AddLights(const vec3_t origin, vec3_t color)
 {
     dlight_t *light;
     vec_t f;
@@ -230,7 +230,7 @@ static void GL_AddLights(vec3_t origin, vec3_t color)
 #define GL_AddLights(origin, color) (void)0
 #endif
 
-void GL_LightPoint(vec3_t origin, vec3_t color)
+void GL_LightPoint(const vec3_t origin, vec3_t color)
 {
     if (gl_fullbright->integer) {
         VectorSet(color, 1, 1, 1);
@@ -251,7 +251,7 @@ void GL_LightPoint(vec3_t origin, vec3_t color)
     }
 }
 
-void R_LightPoint(vec3_t origin, vec3_t color)
+void R_LightPoint(const vec3_t origin, vec3_t color)
 {
     int i;
 

cynicgamesofficial/Quasimodo-RTX

src/refresh/gl/gl.h
@@ -207,9 +207,9 @@ typedef enum {
     CULL_CLIP
 } glCullResult_t;
 
-glCullResult_t GL_CullBox(vec3_t bounds[2]);
+glCullResult_t GL_CullBox(const vec3_t bounds[2]);
 glCullResult_t GL_CullSphere(const vec3_t origin, float radius);
-glCullResult_t GL_CullLocalBox(const vec3_t origin, vec3_t bounds[2]);
+glCullResult_t GL_CullLocalBox(const vec3_t origin, const vec3_t bounds[2]);
 
 bool GL_AllocBlock(int width, int height, int *inuse,
                    int w, int h, int *s, int *t);
@@ -542,8 +542,8 @@ void GL_ClearSolidFaces(void);
 void GL_DrawBspModel(mmodel_t *model);
 void GL_DrawWorld(void);
 void GL_SampleLightPoint(vec3_t color);
-void GL_LightPoint(vec3_t origin, vec3_t color);
-void R_LightPoint_GL(vec3_t origin, vec3_t color);
+void GL_LightPoint(const vec3_t origin, vec3_t color);
+void R_LightPoint_GL(const vec3_t origin, vec3_t color);
 
 /*
  * gl_sky.c
@@ -552,7 +552,7 @@ void R_LightPoint_GL(vec3_t origin, vec3_t color);
 void R_AddSkySurface(mface_t *surf);
 void R_ClearSkyBox(void);
 void R_DrawSkyBox(void);
-void R_SetSky_GL(const char *name, float rotate, vec3_t axis);
+void R_SetSky_GL(const char *name, float rotate, const vec3_t axis);
 
 /*
  * gl_mesh.c

src/refresh/gl/sky.c
@@ -379,7 +379,7 @@ static void R_UnsetSky(void)
 R_SetSky
 ============
 */
-void R_SetSky_GL(const char *name, float rotate, vec3_t axis)
+void R_SetSky_GL(const char *name, float rotate, const vec3_t axis)
 {
     int     i;
     char    pathname[MAX_QPATH];

src/refresh/gl/world.c
@@ -72,7 +72,7 @@ void GL_SampleLightPoint(vec3_t color)
     }
 }
 
-static bool _GL_LightPoint(vec3_t start, vec3_t color)
+static bool _GL_LightPoint(const vec3_t start, vec3_t color)
 {
     bsp_t           *bsp;
     int             i, index;
@@ -213,7 +213,7 @@ static void GL_TransformLights(mmodel_t *model)
     }
 }
 
-static void GL_AddLights(vec3_t origin, vec3_t color)
+static void GL_AddLights(const vec3_t origin, vec3_t color)
 {
     dlight_t *light;
     vec_t f;
@@ -230,7 +230,7 @@ static void GL_AddLights(vec3_t origin, vec3_t color)
     }
 }
 
-void GL_LightPoint(vec3_t origin, vec3_t color)
+void GL_LightPoint(const vec3_t origin, vec3_t color)
 {
     if (gl_fullbright->integer) {
         VectorSet(color, 1, 1, 1);
@@ -251,7 +251,7 @@ void GL_LightPoint(vec3_t origin, vec3_t color)
     }
 }
 
-void R_LightPoint_GL(vec3_t origin, vec3_t color)
+void R_LightPoint_GL(const vec3_t origin, vec3_t color)
 {
     int i;
 