skullernet/q2pro

Add SZ_ReadByte() and SZ_ReadShort().

src/common/sizebuf.c
@@ -143,6 +143,18 @@ void *SZ_ReadData(sizebuf_t *buf, size_t len)
     return buf->data + buf->readcount - len;
 }
 
+int SZ_ReadByte(sizebuf_t *sb)
+{
+    byte *buf = SZ_ReadData(sb, 1);
+    return buf ? *buf : -1;
+}
+
+int SZ_ReadShort(sizebuf_t *sb)
+{
+    byte *buf = SZ_ReadData(sb, 2);
+    return buf ? (int16_t)LittleShortMem(buf) : -1;
+}
+
 int SZ_ReadLong(sizebuf_t *sb)
 {
     byte *buf = SZ_ReadData(sb, 4);

cynicgamesofficial/Quasimodo-RTX

src/common/sizebuf.c
@@ -144,6 +144,18 @@ void *SZ_ReadData(sizebuf_t *buf, size_t len)
     return buf->data + buf->readcount - len;
 }
 
+int SZ_ReadByte(sizebuf_t *sb)
+{
+    byte *buf = SZ_ReadData(sb, 1);
+    return buf ? *buf : -1;
+}
+
+int SZ_ReadShort(sizebuf_t *sb)
+{
+    byte *buf = SZ_ReadData(sb, 2);
+    return buf ? (int16_t)RL16(buf) : -1;
+}
+
 int SZ_ReadLong(sizebuf_t *sb)
 {
     byte *buf = SZ_ReadData(sb, 4);