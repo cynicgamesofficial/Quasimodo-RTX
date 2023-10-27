cynicgamesofficial/Quasimodo-RTX

commit 7e154fe30787354644c5c4fe7cff86d60e554922

Add a hack for action mod scope scaling.

src/client/screen.c
@@ -1600,7 +1600,20 @@ static void SCR_ExecuteLayoutString(const char *s)
             }
             token = cl.configstrings[CS_IMAGES + index];
             if (token[0] && cl.image_precache[index]) {
-                R_DrawPic(x, y, cl.image_precache[index]);
+                qhandle_t pic = cl.image_precache[value];
+                // hack for action mod scope scaling
+                if (x == scr.hud_width  / 2 - 160 &&
+                    y == scr.hud_height / 2 - 120 &&
+                    Com_WildCmp("scope?x", token))
+                {
+                    int w = 320 * ch_scale->value;
+                    int h = 240 * ch_scale->value;
+                    R_DrawStretchPic((scr.hud_width  - w) / 2 + ch_x->integer,
+                                     (scr.hud_height - h) / 2 + ch_y->integer,
+                                     w, h, pic);
+                } else {
+                    R_DrawPic(x, y, pic);
+                }
             }
 
             if (value == STAT_SELECTED_ICON && scr_showitemname->integer)

commit a1108bb6cb9f064ac6deab313b9b777e7484e4fc

Fixed the status bar rendering that was broken in commit 7e154fe30787354644c5c4fe7cff86d60e554922 (MR https://github.com/NVIDIA/Q2RTX/pull/346)

src/client/screen.c
@@ -1600,7 +1600,7 @@ static void SCR_ExecuteLayoutString(const char *s)
             }
             token = cl.configstrings[CS_IMAGES + index];
             if (token[0] && cl.image_precache[index]) {
-                qhandle_t pic = cl.image_precache[value];
+                qhandle_t pic = cl.image_precache[index];
                 // hack for action mod scope scaling
                 if (x == scr.hud_width  / 2 - 160 &&
                     y == scr.hud_height / 2 - 120 &&